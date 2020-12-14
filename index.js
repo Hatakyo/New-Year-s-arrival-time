5177dddaf643a005273f0c97400f94f0f261ce5e
Hatakyo <66242596+Hatakyo@users.noreply.github.com>
2020-12-14 14:14:00 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var newYear = ' 1 January 2021';
+
+function countDown() {
+    var newYearDAy = new Date(newYear);
+    var currentDay = new Date();
+
+    var totalSeconds = (newYearDAy - currentDay) / 1000;
+    var days = Math.floor(totalSeconds / 3600 / 24);
+    var hours = Math.floor(totalSeconds / 3600) % 24;
+    var minutes = (Math.floor(totalSeconds / 60) % 60)
+    var seconds = Math.floor(totalSeconds % 60);
+    // console.log(days, hours, minutes, seconds)
+
+    document.getElementById('days').innerHTML = days;
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
+
+}
+
+countDown();
+
+setInterval(countDown, 1000);
